113136898c27db8d16aaa699359401edd23fbf1c
malikkhalim <akbaribra000@gmail.com>
2024-07-25 09:37:58 +0000

    add node.js and homepage

---
+++ app.js
@@ -0,0 +1,23 @@
+// File: app.js
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// Set the view engine to ejs
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Main route
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
